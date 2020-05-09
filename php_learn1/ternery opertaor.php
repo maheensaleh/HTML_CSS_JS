ae4e9a38f6bff2c494c10ab208b7b6d2fba6cc19
maheensaleh <salehmaheen@gmail.com>
2020-05-10 03:24:00 +0500

    ternery operator

---
+++ php_learn1/ternery opertaor.php
@@ -0,0 +1,18 @@
+<?php 
+// terneray operator is a short hand for making conditional statements 
+
+// as follows
+
+$a = true;
+$b = false;
+
+$final = (($a==true) ? 
+    ($b==false ?  "yess":"noo")
+    :("nops"));
+
+echo "result is $final";
+
+
+
+
+?>
